cae16acfdde4ccb163b79e14f70a456580f4dfcc
ABPD2001 <a.puretemadi@gmail.com>
2026-09-23 13:58:02 +0330

    developing format caster...

--- src/core/caster.cpp
+++ src/core/caster.cpp
@@ -1,21 +1,16 @@
 #include "./caster.hpp"
 
 _GX_CASTER::_GX_CASTER() {};
-void _GX_CASTER::config(string content, _GX_CASTER_format_t format)
+void _GX_CASTER::config(_GX_CASTER_format_t format)
 {
     this->format = format;
-    this->content = content;
 };
-string _GX_CASTER::cast_format(_GX_CASTER_format_t format)
+void _GX_CASTER::update(string content)
 {
-    vector<vector<uint64_t>> snippets;
-
-    // first, read snippets.
-
     if (this->format.binary)
     {
         vector<string> units = split(this->format.format, ',');
-        uint32_t s = 0, n = this->content.size(), o = 0; // by bytes.
+        uint32_t s = 0, n = content.size(), o = 0; // by bytes.
         for (string u : units)
         {
             s += to_uint32(u.substr(1)) / 8;
@@ -23,17 +18,79 @@ string _GX_CASTER::cast_format(_GX_CASTER_format_t format)
 
         for (uint32_t i = 0; n >= s; i++)
         {
-            vector<uint64_t> snipp;
-            string snipp_raw = this->content.substr(i, s);
+            vector<long long int> snipp;
+            string snipp_raw = content.substr(i, s);
             for (string u : units)
             {
                 const uint32_t s_i = to_uint32(u.substr(1)) / 8;
-                snipp.push_back(to_uint64(snipp_raw.substr(o, s_i)));
+                snipp.push_back(to_uint32(snipp_raw.substr(o, s_i)));
                 o += s_i;
             }
-            snippets.push_back(snipp);
+            this->snippets.push_back(snipp);
             o = 0;
             n -= s;
         }
     }
+    else
+    {
+        const vector<string> lines = split(content, '\n');
+        vector<long long int> snipp;
+        if (this->format.seperator)
+        {
+            for (string l : lines)
+            {
+                const vector<string> parts = split(l, this->format.seperator);
+                for (string p : parts)
+                {
+                    snipp.push_back(stoll(p));
+                }
+                this->snippets.push_back(snipp);
+                snipp.clear();
+            }
+        }
+        else
+        {
+            string sep;
+            uint32_t n = 0;
+            bool a = false;
+            for (string l : lines)
+            {
+                sep = "";
+                n = 0;
+                a = false;
+                for (uint32_t i = 0; i < l.length(); i++)
+                {
+
+                    if (l[i] == '>')
+                    {
+                        a = true;
+                        continue;
+                    }
+                    if (l[i] == '<')
+                    {
+                        a = false;
+                        continue;
+                    }
+                    if (a)
+                    {
+                        sep += l[i];
+                        continue;
+                    }
+                    if (sep && !a)
+                    {
+                        const uint32_t idx = l.substr(n).find(sep);
+                        snipp.push_back(stoll(l.substr(n, idx)));
+                        n += idx + sep.length();
+                        sep = "";
+                    }
+                }
+                this->snippets.push_back(snipp);
+                snipp.clear();
+            }
+        }
+    }
+}
+
+string _GX_CASTER::cast_format(_GX_CASTER_format_t format)
+{
 }
--- src/core/caster.hpp
+++ src/core/caster.hpp
@@ -3,29 +3,28 @@
 #include <utils/string.hpp>
 #include <utils/number.hpp>
 
+using std::stoll;
 using std::string;
 using std::vector;
 
 struct _GX_CASTER_format_t
 {
     string format; // or binary logic.
+    string seperator;
     bool binary = false;
     bool endianness = false; // small
 };
 
-struct _GX_CASTER_snippet_txt_t
-{
-    string regname;
-    string value;
-};
 class _GX_CASTER
 {
 private:
+    vector<long long int> snippets;
     string content;
     _GX_CASTER_format_t format;
 
 public:
     _GX_CASTER();
-    void config(string content, _GX_CASTER_format_t format);
+    void config(_GX_CASTER_format_t format);
+    void update(string content);
     string cast_format(_GX_CASTER_format_t format);
 };
